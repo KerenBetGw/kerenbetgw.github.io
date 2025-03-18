36f8d1fbb30c7bc26b330dfce6936bf9565dbed5
KerenBetGw <ryu.paris05@gmail.com>
2025-03-18 20:12:13 +0700

    Add files via upload

---
+++ files/install.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install unzip git -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.bashrc
+nvm install 20.19
+wget https://github.com/KerenBetGw/kerenbetgw.github.io/raw/refs/heads/main/files/V5.zip
+unzip V5.zip
+screen -s install npm i --force
+screen -s nano npm start
